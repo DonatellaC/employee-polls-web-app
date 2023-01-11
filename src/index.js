7f5db26503fc16af21c0c6d6fe73efd2d1707f7a
DonatellaC <donatella.carta@mailbox.org>
2023-01-11 16:25:51 +0100

    add reducers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
 import App from "./components/App";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+const store = createStore(reducer, middleware);
+
+ReactDOM.render(
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
