1308277075291079bfe3c309b78d638be95d9f4a
DonatellaC <donatella.carta@mailbox.org>
2023-01-11 16:49:25 +0100

    add middleware

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import { legacy_createStore as createStore } from "redux";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
+import reducer from "./reducers";
+import middleware from "./middleware";
 import App from "./components/App";
 import "./index.css";
 
 const store = createStore(reducer, middleware);
+const root = createRoot(document.getElementById("root"));
 
-ReactDOM.render(
+root.render(
   <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
