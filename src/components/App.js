046969f39b80a9b0e5847792de0441d8f181183b
DonatellaC <donatella.carta@mailbox.org>
2023-01-11 18:53:24 +0100

    add login condition

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,36 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
 import NewQuestion from "./NewQuestion";
 import Leaderboard from "./Leaderboard";
 import Nav from "./Nav";
 
-const App = () => {
+const App = (props) => {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  }, []);
   return (
     <>
-      <Nav />
-      <Routes>
-        <Route exact path="/" element={<Dashboard />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
-        <Route path="/new-question" element={<NewQuestion />} />
-      </Routes>
+      {props.authedUser === null ? (
+        <h1>Login</h1>
+      ) : (
+        <>
+          <Nav />
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/new-question" element={<NewQuestion />} />
+          </Routes>
+        </>
+      )}
     </>
   );
 };
 
-export default App;
+const mapStateToProps = ({ authedUser }) => ({
+  authedUser,
+});
+
+export default connect(mapStateToProps)(App);
