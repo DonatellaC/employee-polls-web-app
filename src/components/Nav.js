3680e835cc4870fe21aed0d221a162750ea3b7ee
DonatellaC <donatella.carta@mailbox.org>
2023-01-12 13:30:50 +0100

    update nav component + style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,32 @@
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { setAuthedUser } from "../actions/authedUser";
 
-const Nav = ({ name, avatarURL }) => {
+const Nav = ({ name, avatarURL, dispatch }) => {
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(setAuthedUser(null));
+  };
+
   return (
-    <nav className="nav">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/leaderboard">Leaderboard</Link>
-        </li>
-        <li>
-          <Link to="/new-question">New Question</Link>
-        </li>
-      </ul>
-      <div>
-        <div>
-          <img src={avatarURL} alt={name} />
-          <span>{name}</span>
-        </div>
-        <button>Logout</button>
+    <nav className="navbar">
+      <div className="navbar-left">
+        <Link to="/" className="navbar-link">
+          Home
+        </Link>
+        <Link to="/leaderboard" className="navbar-link">
+          Leaderboard
+        </Link>
+        <Link to="/new-question" className="navbar-link">
+          New Question
+        </Link>
+      </div>
+      <div className="navbar-right">
+        <img src={avatarURL} alt={name} className="navbar-avatar" />
+        <span className="navbar-username">{name}</span>
+        <button className="navbar-logout-btn" onClick={handleLogout}>
+          Logout
+        </button>
       </div>
     </nav>
   );
